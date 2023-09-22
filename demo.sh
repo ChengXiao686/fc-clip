825c7c78f18a661970e98e8a4bffaea7c4b71e9f
ChengXiao686 <xiao.cheng@autra.tech>
2023-09-22 12:02:02 +0800

    up

--- demo.sh
+++ demo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python demo/demo.py --input imgs/input/00014384.jpg \
+python demo/demo.py --input imgs/input/00014381.jpg \
   --output imgs/output/ \
   --opts MODEL.WEIGHTS checkpoints/fcclip_cocopan.pth
